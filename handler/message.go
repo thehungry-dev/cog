7fa5d06236b8e87bfd4de802c8f322a0b5057417
Francesco Belladonna <francesco@fc5.me>
2021-03-28 16:11:22 -0700

    Message tags can now be replaced

--- handler/message.go
+++ handler/message.go
@@ -13,11 +13,14 @@ func (msg Message) Halt() Message {
 	msg.halted = true
 	return msg
 }
-
 func (msg Message) IsHalted() bool {
 	return msg.halted
 }
 
 func (msg Message) Tags() []string {
 	return msg.tags
+}
+func (msg Message) SetTags(tags []string) Message {
+	msg.tags = tags
+	return msg
 }
