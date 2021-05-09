feae7e09df04967eb2560d5caf8bc0442255fb3b
Francesco Belladonna <francesco@fc5.me>
2021-05-08 21:22:57 -0700

    Tag output and output type can now be controlled through environment variables

--- settings/builder.go
+++ settings/builder.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/thehungry-dev/log/handler"
+	"github.com/thehungry-dev/log/message/transform"
 )
 
 const DefaultTagFilterText = "_all"
@@ -14,18 +15,22 @@ type nullStringWriter struct{}
 func (nullStringWriter) WriteString(_ string) (int, error) { return 0, nil }
 
 type Builder struct {
-	TagFilterText   string
-	LevelFilterText string
-	DeviceText      string
-	FormattersText  string
+	TagFilterText    string
+	LevelFilterText  string
+	DeviceText       string
+	FormattersText   string
+	TagsOutputText   string
+	OutputFormatText string
 }
 
 func Getenv() Builder {
 	return Builder{
-		TagFilterText:   os.Getenv("LOG_TAGS"),
-		LevelFilterText: os.Getenv("LOG_LEVEL"),
-		DeviceText:      os.Getenv("LOG_DEVICE"),
-		FormattersText:  os.Getenv("LOG_FORMATTERS"),
+		TagFilterText:    os.Getenv("LOG_TAGS"),
+		LevelFilterText:  os.Getenv("LOG_LEVEL"),
+		DeviceText:       os.Getenv("LOG_DEVICE"),
+		FormattersText:   os.Getenv("LOG_FORMATTERS"),
+		TagsOutputText:   os.Getenv("LOG_TAGS_OUTPUT"),
+		OutputFormatText: os.Getenv("LOG_OUTPUT_FORMAT"),
 	}
 }
 
@@ -39,6 +44,17 @@ func (builder Builder) LevelFilter() handler.Handler {
 		return handler.Passthrough
 	}
 }
+func (builder Builder) OutputHandler() handler.Handler {
+	config := transform.DefaultConfig
+	config.Tags = builder.OutputTags()
+
+	return handler.Output{
+		Device: builder.Device(),
+		Format: builder.OutputFormat(),
+		Config: config,
+	}
+}
+func (builder Builder) OutputTags() bool { return builder.TagsOutputText == "on" }
 func (builder Builder) Formatters() bool { return builder.FormattersText == "on" }
 func (builder Builder) Device() io.StringWriter {
 	var output io.StringWriter
@@ -55,4 +71,20 @@ func (builder Builder) Device() io.StringWriter {
 	}
 
 	return output
+}
+func (builder Builder) OutputFormat() handler.OutputFormat {
+	var format handler.OutputFormat
+
+	switch builder.OutputFormatText {
+	case "json":
+		format = handler.OutputJSON
+	case "text":
+		fallthrough
+	case "":
+		fallthrough
+	default:
+		format = handler.OutputText
+	}
+
+	return format
 }
